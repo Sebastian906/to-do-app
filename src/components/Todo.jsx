a7721b206fdcb061b230968ada4795f490daa877
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-08-22 08:14:29 -0500

    Se añade la función de añadir y escribir notas por si mismo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
 const Todo = () => {
+
+    const [todoList, setTodoList] = useState([]);
+
+    const inputRef = useRef();
+
+    const add = () => {
+        const inputText = inputRef.current.value.trim();
+        if (inputText === "") {
+            return null;
+        }
+        const newTodo = {
+            id: Date.now(),
+            text: inputText,
+            isComplete: false,
+        }
+        setTodoList((prev) => [...prev, newTodo]);
+        inputRef.current.value = "";
+    }
+
     return (
         <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
             <div className='flex items-center mt-7 gap-2'>
@@ -11,13 +30,14 @@ const Todo = () => {
             </div>
 
             <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-                <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Añada tarea' />
-                <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Añadir +</button>
+                <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Añada tarea' />
+                <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Añadir +</button>
             </div>
 
             <div>
-                <TodoItems text='Test'/>
-                <TodoItems text='Ir a Clase'/>
+                {todoList.map((item, index) => {
+                    return <TodoItems key={index} text={item.text}/>
+                })}
             </div>
         </div>
     )
